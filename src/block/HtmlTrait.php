ec52c69478d2d823235c0b9a0f6079c003eea9cd
Daniel Pimley <dan@pimley.net>
2025-08-30 15:43:51 +0100

    HtmlTrait: escape regex hashes

--- src/block/HtmlTrait.php
+++ src/block/HtmlTrait.php
@@ -232,7 +232,7 @@ trait HtmlTrait
 	{
 		if (
 			preg_match(
-				'/^&(#[\d]{1,7}|#[x][a-f0-9]{1,6}|[\w\d]{2,});/i',
+				'/^&(\#[\d]{1,7}|\#[x][a-f0-9]{1,6}|[\w\d]{2,});/i',
 				$text,
 				$matches
 			)
