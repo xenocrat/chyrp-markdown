6bf916f1589bbc63843519ff37318362c2f66858
Daniel Pimley <dan@pimley.net>
2026-01-03 16:37:15 +0000

    Updating copyright notices

--- src/block/FootnoteTrait.php
+++ src/block/FootnoteTrait.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright Copyright 2014 Carsten Brandt, 2024 Daniel Pimley and other contributors
+ * @copyright Copyright 2014 Carsten Brandt, 2024-2026 Daniel Pimley and other contributors
  * @license https://github.com/xenocrat/chyrp-markdown/blob/master/LICENSE
  * @link https://github.com/xenocrat/chyrp-markdown#readme
  */
