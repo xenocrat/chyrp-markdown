36415724c57870fba31025e753c26cddfa3219f8
Daniel Pimley <dan@pimley.net>
2026-04-24 13:11:11 +0100

    EmphStrongTrait: use detectOvershoot()

--- src/inline/EmphStrongTrait.php
+++ src/inline/EmphStrongTrait.php
@@ -87,32 +87,11 @@ trait EmphStrongTrait
 				);
 				$content = $matches[1];
 				if (
-					// Inline HTML takes precedence.
-					(
-						!method_exists($this, 'parseLt')
-						|| ($pos = strpos($content, $this->parseLtMarkers()[0]))
-							=== false
-						|| ($arr = $this->parseLt(substr($markdown, (2 + $pos))))[0][0]
-							=== 'text'
-						|| $arr[1] <= (strlen($content) - $pos)
-					)
-					// Inline link takes precedence.
-					&& (
-						!method_exists($this, 'parseLink')
-						|| ($pos = strpos($content, $this->parseLinkMarkers()[0]))
-							=== false
-						|| ($arr = $this->parseLink(substr($markdown, (2 + $pos))))[0][0]
-							=== 'text'
-						|| $arr[1] <= (strlen($content) - $pos)
-					)
-					// Inline image takes precedence.
-					&& (
-						!method_exists($this, 'parseImage')
-						|| ($pos = strpos($content, $this->parseImageMarkers()[0]))
-							=== false
-						|| ($arr = $this->parseImage(substr($markdown, (2 + $pos))))[0][0]
-							=== 'text'
-						|| $arr[1] <= (strlen($content) - $pos)
+					// Inline HTML, link, or image takes precedence.
+					!$this->detectOvershoot(
+						$markdown,
+						strlen($matches[0]),
+						['Lt', 'Link', 'Image']
 					)
 				) {
 					return [
@@ -183,32 +162,11 @@ trait EmphStrongTrait
 				);
 				$content = $matches[1];
 				if (
-					// Inline HTML takes precedence.
-					(
-						!method_exists($this, 'parseLt')
-						|| ($pos = strpos($content, $this->parseLtMarkers()[0]))
-							=== false
-						|| ($arr = $this->parseLt(substr($markdown, (1 + $pos))))[0][0]
-							=== 'text'
-						|| $arr[1] <= (strlen($content) - $pos)
-					)
-					// Inline link takes precedence.
-					&& (
-						!method_exists($this, 'parseLink')
-						|| ($pos = strpos($content, $this->parseLinkMarkers()[0]))
-							=== false
-						|| ($arr = $this->parseLink(substr($markdown, (1 + $pos))))[0][0]
-							=== 'text'
-						|| $arr[1] <= (strlen($content) - $pos)
-					)
-					// Inline image takes precedence.
-					&& (
-						!method_exists($this, 'parseImage')
-						|| ($pos = strpos($content, $this->parseImageMarkers()[0]))
-							=== false
-						|| ($arr = $this->parseImage(substr($markdown, (1 + $pos))))[0][0]
-							=== 'text'
-						|| $arr[1] <= (strlen($content) - $pos)
+					// Inline HTML, link, or image takes precedence.
+					!$this->detectOvershoot(
+						$markdown,
+						strlen($matches[0]),
+						['Lt', 'Link', 'Image']
 					)
 				) {
 					return [
