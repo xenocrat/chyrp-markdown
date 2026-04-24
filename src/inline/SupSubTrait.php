f4fa3c96dfe1dd1aa5d7cee42464dc5763cf9155
Daniel Pimley <dan@pimley.net>
2026-04-24 13:11:53 +0100

    SupSubTrait: use detectOvershoot()

--- src/inline/SupSubTrait.php
+++ src/inline/SupSubTrait.php
@@ -47,32 +47,11 @@ trait SupSubTrait
 			);
 			$content = $matches[1];
 			if (
-				// Inline HTML takes precedence.
-				(
-					!method_exists($this, 'parseLt')
-					|| ($pos = strpos($content, $this->parseLtMarkers()[0]))
-						=== false
-					|| ($arr = $this->parseLt(substr($markdown, (2 + $pos))))[0][0]
-						=== 'text'
-					|| $arr[1] <= (strlen($content) - $pos)
-				)
-				// Inline link takes precedence.
-				&& (
-					!method_exists($this, 'parseLink')
-					|| ($pos = strpos($content, $this->parseLinkMarkers()[0]))
-						=== false
-					|| ($arr = $this->parseLink(substr($markdown, (2 + $pos))))[0][0]
-						=== 'text'
-					|| $arr[1] <= (strlen($content) - $pos)
-				)
-				// Inline image takes precedence.
-				&& (
-					!method_exists($this, 'parseImage')
-					|| ($pos = strpos($content, $this->parseImageMarkers()[0]))
-						=== false
-					|| ($arr = $this->parseImage(substr($markdown, (2 + $pos))))[0][0]
-						=== 'text'
-					|| $arr[1] <= (strlen($content) - $pos)
+				// Inline HTML, link, or image takes precedence.
+				!$this->detectOvershoot(
+					$markdown,
+					strlen($matches[0]),
+					['Lt', 'Link', 'Image']
 				)
 			) {
 				return [
